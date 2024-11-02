4df91938c5a3e594d6307a525d842677902459be
Matyas Angyal <matyasjay@gmail.com>
2024-11-02 14:38:10 +0000

    Built and published by Moonwave

--- assets/js/b3547cfc.934932c3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[212],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/matyasjay/campingsite/blob/main","baseUrl":"/campingsite/","classOrder":[],"apiCategories":[]}')}}]);
--- assets/js/c17d023a.a11694c6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[187],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Animate","label":"\u200bAnimate"},{"type":"link","href":"/api/Persona","label":"\u200bPersona"},{"type":"link","href":"/api/StatsGui","label":"\u200bStats\u200bGui"},{"type":"link","href":"/api/Trigger","label":"\u200bTrigger"}]')}}]);
