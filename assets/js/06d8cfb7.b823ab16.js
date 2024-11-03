499c01f426543221326d5b3f9b3e13a24b6c94fb
Matyas Angyal <matyasjay@gmail.com>
2024-11-03 12:40:35 +0000

    Built and published by Moonwave

---
+++ assets/js/06d8cfb7.b823ab16.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[767],{85193:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Variable","id":"Variable","level":3},{"value":"Properties","id":"properties","level":2},{"value":"variables","id":"variables","level":3},{"value":"Functions","id":"functions","level":2}]')}}]);
